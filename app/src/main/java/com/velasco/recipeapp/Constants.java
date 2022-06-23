cfac680d72d932865d6ec120b8437f104f91d042
urmrs98 <paolavlsc170698@gmail.com>
2022-06-23 18:41:36 +0300

    Category php connection works

---
+++ app/src/main/java/com/velasco/recipeapp/Constants.java
@@ -0,0 +1,15 @@
+package com.velasco.recipeapp;
+
+public class Constants {
+
+    public static final String URL= "http://10.0.2.2/";
+
+    public static final String ROOT_URL = URL + "Recipes/";
+    public static final String URL_SELECT_CATEGORIES = ROOT_URL + "select_categories.php";
+
+    public static final String URL_ADD = ROOT_URL + "add.php";
+    public static final String URL_DELETE = ROOT_URL + "delete.php";
+    public static final String URL_UPDATE = ROOT_URL + "update.php";
+    public static final String URL_SELECT = ROOT_URL + "select.php";
+    public static final String URL_SELECT_ID = ROOT_URL + "select_id.php";
+}
