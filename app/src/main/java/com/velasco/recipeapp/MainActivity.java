e783e00020a31adf808d53906c35aa9ff59b5f76
urmrs98 <paolavlsc170698@gmail.com>
2022-06-23 14:52:01 +0300

    Fragment Manager

--- app/src/main/java/com/velasco/recipeapp/MainActivity.java
+++ app/src/main/java/com/velasco/recipeapp/MainActivity.java
@@ -1,15 +1,52 @@
 package com.velasco.recipeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
 
-
+    // STEP 6: Sundesh me xml
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // STEP 9: Να ξεκινάει από εδώ
+        replaceFragment(new CategoriesFragment()); // at the start of the app
+
+        // STEP 7: sundesh
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        // STEP 8: on click navigate
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home:
+                    replaceFragment(new CategoriesFragment());
+                    break;
+                case R.id.add:
+                    replaceFragment(new AddFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    // STEP 10: Βοηθητική συνάρτηση
+    // the main activity is repsonsible for the switches on fragments
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager(); // diaxeirisths
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction(); // sunallagh
+        fragmentTransaction.replace(R.id.frameLayout, fragment); // pou tha ginei h sunallagh
+        fragmentTransaction.commit(); // commit
     }
 }
