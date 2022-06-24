a60027fcc306e4b5c957e7948ee411dc3b85ed2d
urmrs98 <paolavlsc170698@gmail.com>
2022-06-25 02:03:37 +0300

    Add cancel

--- app/src/main/java/com/velasco/recipeapp/AddFragment.java
+++ app/src/main/java/com/velasco/recipeapp/AddFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,10 +56,30 @@ public class AddFragment extends Fragment {
         }
     }
 
+
+    View view;
+    private Button cancelBtn;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false);
+        view = inflater.inflate(R.layout.fragment_add, container, false);
+
+        cancelBtn = view.findViewById(R.id.btn_cancelRecipe);
+
+        // cancel
+        cancelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                CategoriesFragment categoriesFragment = new CategoriesFragment();
+                getParentFragmentManager().beginTransaction().replace(R.id.addFrag, categoriesFragment).commit();
+            }
+        });
+
+
+
+        return view;
     }
 }
